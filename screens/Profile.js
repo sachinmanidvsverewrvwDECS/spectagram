876f060e007e1e7dfb31c4b1089896e6956532fc
sachinmanidvsverewrvwDECS <90509273+sachinmanidvsverewrvwDECS@users.noreply.github.com>
2021-12-25 23:53:52 -0800

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+export default class Profile extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text>Profile</Text>
+            </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+})
